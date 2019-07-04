083630369128d1772d8eb6ff4a67db907cdb8a62
Atesh <atesh.koul@gmail.com>
2019-07-04 16:24:16 +0200

    Update docker_win.cmd

--- Experimental/Docker/docker_win.cmd
+++ Experimental/Docker/docker_win.cmd
@@ -1,3 +1,10 @@
+@ECHO OFF
 start "" "docker.exe" pull ateshkoul/predpsych
-start "" "docker.exe" run --rm -p 8787:8787 -e PASSWORD=predpsych ateshkoul/predpsych
+timeout 2 /nobreak > nul
+start "" "docker.exe" run --name "predpsych" --rm -p 8787:8787 -e PASSWORD=predpsych ateshkoul/predpsych 
+timeout 2 /nobreak > nul
 start "" http://localhost:8787/
+timeout 3 /nobreak > nul
+ECHO "Stop Container?"
+PAUSE
+start "" "docker.exe" stop predpsych 
